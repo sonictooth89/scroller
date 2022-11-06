3049405fa0171fc6c0e5f5b35e6575589fb8ab58
Malgorzata Nowak <nowak.em@interia.eu>
2022-11-06 21:17:57 +0100

    Add Scroller class and use it

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Helo, World!");
+    const scroller = new Scroller('#root');
 
-    const rootElement = document.querySelector('#root');
-    const sections = document.querySelectorAll('section');
-    let currentSectionIndex = 0;
-    let isThrottled = false;
-
-    setTimeout(function(){
-        isThrottled = false;
-    }, 1000);
-
-    console.log(sections);
-
-    document.addEventListener('mousewheel', function(event) {
-        if (isThrottled) return;
-        isThrottled = true;
-        const direction = event.wheelDelta < 0 ? 1 : -1;
-
-        scroll();
-        
-        currentSectionIndex = currentSectionIndex + direction;
-
-        scrollToCurrentSection();
-    });
+    document.addEventListener('mousewheel', scroller.listenScroll);
 
     function scroll(direction){
         if (direction === 1) {
